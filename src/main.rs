04e638f1e39e6391e66486b1e8c6f4e90ab43b84
Blake Hawkins <blake.hawkins.11@gmail.com>
2019-06-11 20:52:21 +0100

    make current

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ extern crate glutin;
 
 fn main() {
     let mut el = glutin::EventsLoop::new();
+
     let wb = glutin::WindowBuilder::new()
         .with_title("FFPS - Fuck-yeah FPS")
         .with_dimensions(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-    let windowed_context = glutin::ContextBuilder::new()
-        .build_windowed(wb, &el)
-        .unwrap();
+
+    let windowed_context = {
+        let ctxt = glutin::ContextBuilder::new()
+            .build_windowed(wb, &el)
+            .expect("Create context fail");
+
+        let ctxt = unsafe { ctxt.make_current().expect("Make current fail") };
+
+        ctxt
+    };
 
     el.run_forever(|e: glutin::Event| {
         println!("got event {:?}", e);
         glutin::ControlFlow::Continue
     });
-
 }
