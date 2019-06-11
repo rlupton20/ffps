8ff5db22b6f35d4aab17e1aff7a91a65a7b2ebba
Richard Lupton <richard.lupton@gmail.com>
2019-06-11 22:33:59 +0100

    Remove whitespace

--- src/main.rs
+++ src/main.rs
@@ -19,101 +19,101 @@ impl Gl {
     }
 }
 
-const VS_SRC: &'static [u8] = b"                                                
-#version 100                                                                    
-precision mediump float;                                                        
-                                                                                
-attribute vec2 position;                                                        
-attribute vec3 color;                                                           
-                                                                                
-varying vec3 v_color;                                                           
-                                                                                
-void main() {                                                                   
-    gl_Position = vec4(position, 0.0, 1.0);                                     
-    v_color = color;                                                            
-}                                                                               
+const VS_SRC: &'static [u8] = b"
+#version 100
+precision mediump float;
+
+attribute vec2 position;
+attribute vec3 color;
+
+varying vec3 v_color;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_color = color;
+}
 \0";
 
-const FS_SRC: &'static [u8] = b"                                                
-#version 100                                                                    
-precision mediump float;                                                        
-                                                                                
-varying vec3 v_color;                                                           
-                                                                                
-void main() {                                                                   
-    gl_FragColor = vec4(v_color, 1.0);                                          
-}                                                                               
+const FS_SRC: &'static [u8] = b"
+#version 100
+precision mediump float;
+
+varying vec3 v_color;
+
+void main() {
+    gl_FragColor = vec4(v_color, 1.0);
+}
 \0";
 
-static VERTEX_DATA: [f32; 15] = [                                               
-    -0.5, -0.5,  1.0,  0.0,  0.0,                                               
-     0.0,  0.5,  0.0,  1.0,  0.0,                                               
-     0.5, -0.5,  0.0,  0.0,  1.0,                                               
+static VERTEX_DATA: [f32; 15] = [
+    -0.5, -0.5,  1.0,  0.0,  0.0,
+     0.0,  0.5,  0.0,  1.0,  0.0,
+     0.5, -0.5,  0.0,  0.0,  1.0,
 ];
 
 unsafe fn shaders(gl: &gl::Gl) {
-    let vs = gl.CreateShader(gl::VERTEX_SHADER);                               
-    gl.ShaderSource(                                                           
-        vs,                                                                    
-        1,                                                                     
-        [VS_SRC.as_ptr() as *const _].as_ptr(),                                
-        std::ptr::null(),                                                      
-    );                                                                         
-    gl.CompileShader(vs);                                                      
-                                                                               
-    let fs = gl.CreateShader(gl::FRAGMENT_SHADER);                             
-    gl.ShaderSource(                                                           
-        fs,                                                                    
-        1,                                                                     
-        [FS_SRC.as_ptr() as *const _].as_ptr(),                                
-        std::ptr::null(),                                                      
-    );                                                                         
-    gl.CompileShader(fs);                                                      
-                                                                               
-    let program = gl.CreateProgram();                                          
-    gl.AttachShader(program, vs);                                              
-    gl.AttachShader(program, fs);                                              
-    gl.LinkProgram(program);                                                   
-    gl.UseProgram(program);                                                    
-                                                                               
-    let mut vb = std::mem::uninitialized();                                    
-    gl.GenBuffers(1, &mut vb);                                                 
-    gl.BindBuffer(gl::ARRAY_BUFFER, vb);                                       
-    gl.BufferData(                                                             
-        gl::ARRAY_BUFFER,                                                      
-        (VERTEX_DATA.len() * std::mem::size_of::<f32>())                       
-            as gl::types::GLsizeiptr,                                          
-        VERTEX_DATA.as_ptr() as *const _,                                      
-        gl::STATIC_DRAW,                                                       
-    );                                                                         
-                                                                               
-    if gl.BindVertexArray.is_loaded() {                                        
-        let mut vao = std::mem::uninitialized();                               
-        gl.GenVertexArrays(1, &mut vao);                                       
-        gl.BindVertexArray(vao);                                               
-    }                                                                          
-                                                                               
-    let pos_attrib =                                                           
-        gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);  
-    let color_attrib =                                                         
-        gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);        
-    gl.VertexAttribPointer(                                                    
-        pos_attrib as gl::types::GLuint,                                       
-        2,                                                                     
-        gl::FLOAT,                                                             
-        0,                                                                     
-        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,                  
-        std::ptr::null(),                                                      
-    );                                                                         
-    gl.VertexAttribPointer(                                                    
-        color_attrib as gl::types::GLuint,                                     
-        3,                                                                     
-        gl::FLOAT,                                                          
-        0,                                                                  
-        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,               
-        (2 * std::mem::size_of::<f32>()) as *const () as *const _,          
-    );                                                                      
-    gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);            
+    let vs = gl.CreateShader(gl::VERTEX_SHADER);
+    gl.ShaderSource(
+        vs,
+        1,
+        [VS_SRC.as_ptr() as *const _].as_ptr(),
+        std::ptr::null(),
+    );
+    gl.CompileShader(vs);
+
+    let fs = gl.CreateShader(gl::FRAGMENT_SHADER);
+    gl.ShaderSource(
+        fs,
+        1,
+        [FS_SRC.as_ptr() as *const _].as_ptr(),
+        std::ptr::null(),
+    );
+    gl.CompileShader(fs);
+
+    let program = gl.CreateProgram();
+    gl.AttachShader(program, vs);
+    gl.AttachShader(program, fs);
+    gl.LinkProgram(program);
+    gl.UseProgram(program);
+
+    let mut vb = std::mem::uninitialized();
+    gl.GenBuffers(1, &mut vb);
+    gl.BindBuffer(gl::ARRAY_BUFFER, vb);
+    gl.BufferData(
+        gl::ARRAY_BUFFER,
+        (VERTEX_DATA.len() * std::mem::size_of::<f32>())
+            as gl::types::GLsizeiptr,
+        VERTEX_DATA.as_ptr() as *const _,
+        gl::STATIC_DRAW,
+    );
+
+    if gl.BindVertexArray.is_loaded() {
+        let mut vao = std::mem::uninitialized();
+        gl.GenVertexArrays(1, &mut vao);
+        gl.BindVertexArray(vao);
+    }
+
+    let pos_attrib =
+        gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
+    let color_attrib =
+        gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
+    gl.VertexAttribPointer(
+        pos_attrib as gl::types::GLuint,
+        2,
+        gl::FLOAT,
+        0,
+        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+        std::ptr::null(),
+    );
+    gl.VertexAttribPointer(
+        color_attrib as gl::types::GLuint,
+        3,
+        gl::FLOAT,
+        0,
+        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+        (2 * std::mem::size_of::<f32>()) as *const () as *const _,
+    );
+    gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
     gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
 }
 
