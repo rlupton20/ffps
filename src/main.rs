16d84a422fe6873030936bd04b2098c7015170cf
Richard Lupton <richard.lupton@gmail.com>
2019-06-11 20:47:11 +0100

    Get it running WOOP WOOP

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ extern crate glutin;
 
 
 fn main() {
-    let el = glutin::EventsLoop::new();
+    let mut el = glutin::EventsLoop::new();
     let wb = glutin::WindowBuilder::new()
         .with_title("FFPS - Fuck-yeah FPS")
         .with_dimensions(glutin::dpi::LogicalSize::new(1024.0, 768.0));
     let windowed_context = glutin::ContextBuilder::new()
         .build_windowed(wb, &el)
         .unwrap();
-    println!("Hello, world!");
+
+    el.run_forever(|e: glutin::Event| {
+        println!("got event {:?}", e);
+        glutin::ControlFlow::Continue
+    });
+
 }
